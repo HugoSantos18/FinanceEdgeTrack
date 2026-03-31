f6ae4094b3629c81c8cd738183f21577769a5059
HugoSantos18 <hugossilva.dev@gmail.com>
2026-03-31 09:51:00 -0300

    feat(env): Variáveis de ambiente

--- FinanceEdgeTrack/Domain/Interfaces/Services/Dashboard/IDashboardService.cs
+++ FinanceEdgeTrack/Domain/Interfaces/Services/Dashboard/IDashboardService.cs
@@ -2,7 +2,5 @@
 
 public interface IDashboardService
 {
-    // verificar quais métodos necessários para fazer o dashboard, o que precisa ser feito e se consigo realizar o uso de API externa
-    // Dashboard --> Somente leitura, não altera estado, não faz Commit nem instancias de BD, utiliza somente os repositories.
-
+    // Métodos dashboard consolidado - TODO
 }
